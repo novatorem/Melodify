8cb3d9cd6014aaae19d8c16ef5b7ecb5236e5676
Andrew Novac <Novac@CastGroup.Local>
2020-01-09 18:14:01 -0500

    Auth & sample call

---
+++ Melodify/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Melodify
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SpotifyAPI spotAPI = new SpotifyAPI("b875781a51d540039acb8fd0aab33e11", "ddc0ef0527744d0d8024448f803de52d");
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var _spotify = new SpotifyWebAPI()
+            {
+                AccessToken = (string)App.Current.Properties["AccessToken"],
+                TokenType = (string)App.Current.Properties["TokenType"]
+            };
+            FullTrack track = _spotify.GetTrack("3Hvu1pq89D4R0lyPBoujSv");
+            System.Diagnostics.Debug.WriteLine(track.Name);
+        }
+    }
+}
---
+++ Melodify/Spotify/SpotifyAPI.cs
@@ -0,0 +1,49 @@
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melodify
+{
+    public class SpotifyAPI
+    {
+        private string _clientId;
+        private string _secretId;
+
+        public SpotifyAPI() { }
+
+        public SpotifyAPI(string clientId, string secretId, string redirectUrl = "http://localhost:4002")
+        {
+            _clientId = clientId;
+            _secretId = secretId;
+
+            if (!string.IsNullOrEmpty(_clientId) && !string.IsNullOrEmpty(_secretId))
+            {
+                ImplicitGrantAuth auth = new ImplicitGrantAuth(
+                    _clientId,
+                    redirectUrl,
+                    redirectUrl,
+                    Scope.UserReadPrivate | Scope.UserReadCurrentlyPlaying
+                );
+
+                auth.AuthReceived += (sender, payload) =>
+                {
+                    auth.Stop();
+                    SpotifyWebAPI api = new SpotifyWebAPI()
+                    {
+                        TokenType = payload.TokenType,
+                        AccessToken = payload.AccessToken
+                    };
+                    App.Current.Properties["TokenType"] = api.TokenType;
+                    App.Current.Properties["AccessToken"] = api.AccessToken;
+                };
+                auth.Start();
+                auth.OpenBrowser();
+            }
+        }
+    }
+}
